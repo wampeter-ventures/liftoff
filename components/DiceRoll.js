053f4b1ca3b909f961c8e94bcd36d7a69853f0b6
dangerberg <danackermangreenberg@gmail.com>
2025-06-06 01:07:49 -0700

    Improve dice instructions layout

--- components/DiceRoll.js
+++ components/DiceRoll.js
@@ -125,15 +125,25 @@ function DiceRoll({ dice, onDragStart, selectedDie, onSelectDie, rocketGrid, roc
                     });
                     
                     if (hasValidPlacements) {
-                        return `Click or drag dice:
-⚙️ Body part (1-5) 🔋 Boosters (6)`;
+                        return (
+                            <>
+                                Click or drag dice:
+                                <br />⚙️ Body part (1-5)
+                                <br />🔋 Boosters (6)
+                            </>
+                        );
                     } else if (!anyDicePlaced) {
                         // Only show fire message if this is a fresh roll with no eligible dice
                         return "No eligible body parts or boosters, send a die to the fire! 🔥 ";
                     } else {
                         // Some dice were already placed, show regular message
-                        return `Click or drag dice:
-⚙️ Body part (1-5) 🔋 Boosters (6)`;
+                        return (
+                            <>
+                                Click or drag dice:
+                                <br />⚙️ Body part (1-5)
+                                <br />🔋 Boosters (6)
+                            </>
+                        );
                     }
                 })()}
             </div>
